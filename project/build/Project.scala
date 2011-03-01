e75fb5181b4bacc3a8e487ee2ef7aac846870364
dev <dev@buffalo.local>
2011-03-01 17:56:48 +0000

    changed format of timestamp to allow easier grouping - again

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,5 +1,9 @@
+import com.github.retronym.OneJarProject
 import sbt._
-class build(info: ProjectInfo) extends DefaultProject(info) {
+
+class build(info: ProjectInfo) extends DefaultProject(info) with OneJarProject {
+
+  override def mainClass = Some("BuildTimerServer")
 
   val casbah = "com.mongodb.casbah" %% "casbah" % "2.0.1"
 
--- src/main/scala/BuildTimerServer.scala
+++ src/main/scala/BuildTimerServer.scala
@@ -1,15 +1,17 @@
 import actors.Actor
+import com.mongodb.casbah.commons.conversions.scala.RegisterJodaTimeConversionHelpers
 import io.Source
 import java.io.IOException
 import java.net.{Socket, ServerSocket}
+import org.joda.time.DateTime
 import com.mongodb.casbah.Imports._
-import java.util.Date
+
 object BuildTimerServer extends Application {
   val port = 10999
 
   try {
-    val listener = new ServerSocket(port)
     val persistence = new BuildTimePersistence("localhost")
+    val listener = new ServerSocket(port)
 
     println("BuildTimerServer listening on port " + port)
 
@@ -20,13 +22,17 @@ object BuildTimerServer extends Application {
     listener.close()
 
   } catch {
-    case e: IOException =>
+    case ioe: IOException =>
       System.err.println("Could not listen on port " + port)
+    case e: com.mongodb.MongoInternalException => {
+      System.err.println("Could not connect to mongo")
+      e.printStackTrace
+    }
   }
 }
 
 class ConnectionHandler(socket: Socket, persistence: BuildTimePersistence) extends Actor {
-  val buildLogPattern = """user:(\w+) host:(\w+) directory:([\w\/]+) command:'(.*)' time:(\d+) processor:([\d\.]+)% memory:(\d+) input:(\d+) output:(\d+) received:(\d+) sent:(\d+) waits:(\d+) os:(\w+) cpu:([\w\d]+) ncpu:(\d+)""".r
+  val buildLogPattern = """user:(\w+) host:(\w+) directory:([\w\-\/]+) command:'(.*)' time:(\d+) processor:([\d\.]+)% memory:(\d+) input:(\d+) output:(\d+) received:(\d+) sent:(\d+) waits:(\d+) os:(\w+) cpu:([\w\d]+) ncpu:(\d+)""".r
 
   def act {
     try {
@@ -37,12 +43,8 @@ class ConnectionHandler(socket: Socket, persistence: BuildTimePersistence) exten
         case buildLogPattern(user, host, directory, command, time, processor, memory, input, output, received, sent, waits, os, cpu, ncpu) =>
           persistence.writeBuildTime(user, host, directory, command, time.toInt)
         case _ =>
-          println("got some funny request: " + input)
+          println("  - got some funny request: " + input)
       }
-
-    } catch {
-      case e =>
-        e.printStackTrace
 
     } finally {
       socket.close()
@@ -51,11 +53,13 @@ class ConnectionHandler(socket: Socket, persistence: BuildTimePersistence) exten
 }
 
 class BuildTimePersistence(host: String) {
+  RegisterJodaTimeConversionHelpers()
 
   val mongo = MongoConnection(host)
   val builds = mongo("buildtime")("builds")
 
   def writeBuildTime(user: String, host: String, directory: String, command: String, elapsedTime: Int) {
+    val now = new DateTime
 
     val builder = MongoDBObject.newBuilder
     builder += "user" -> user
@@ -63,7 +67,12 @@ class BuildTimePersistence(host: String) {
     builder += "directory" -> directory
     builder += "command" -> command
     builder += "elapsedTime" -> elapsedTime
-    builder += "timestamp" -> new Date
+    builder += "timestamp" ->
+      ("year" -> now.year.getAsString,
+        "month" -> now.monthOfYear.getAsString,
+        "day" -> now.dayOfMonth.getAsString,
+        "hour" -> now.hourOfDay.getAsString,
+        "minute" -> now.minuteOfHour.getAsString)
 
     builds += builder.result.asDBObject
 
